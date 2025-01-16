01f303430336f5b0d9d6d251885a851e0d9f156b
junpreetham <arjunpreetham49@gmail.com>
2025-01-16 19:22:04 +0800

    Change of readme file location

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
 setup(
     name="relayx_py",
     version="1.0.1",
@@ -7,5 +11,6 @@ setup(
     install_requires=["nats-py", "pytest-asyncio"],
     author="Relay",
     description="A SDK to connect to the Relay Network",
-    license="Apache 2.0"
+    license="Apache 2.0",
+    long_description=long_description,
 )

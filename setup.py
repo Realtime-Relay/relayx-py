a96aa6a47017823475fca91beb12a06e21902e33
junpreetham <arjunpreetham49@gmail.com>
2025-01-16 19:18:31 +0800

    Change of readme file location

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="relayx_py",
-    version="1.0.0",
+    version="1.0.1",
     packages=["relayx_py"],
     install_requires=["nats-py", "pytest-asyncio"],
     author="Relay",

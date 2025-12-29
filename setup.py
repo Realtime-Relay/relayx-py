ac44cd1120dd51013f6340ba93e33bc45235da18
junpreetham <arjunpreetham49@gmail.com>
2025-12-29 23:02:53 +0530

    V1.1.0: Queues, KV Store, Improved logging

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="relayx_py",
-    version="1.0.15",
+    version="1.1.0",
     packages=find_packages(),
     install_requires=["nats-py==2.12.0", "pytest-asyncio==1.0.0", "nkeys==0.2.1", "msgpack==1.1.1", "tzlocal==5.3.1", "tabulate==0.9.0"],
     author="Relay",
